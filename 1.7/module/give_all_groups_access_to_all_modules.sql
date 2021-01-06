abe5e4e8b5ca5097c0f755dcfa4cd96ae921f361
DelecroixQuentin <DelecroixQuentin@users.noreply.github.com>
2021-01-06 17:57:11 +0100

    Add truncate before Insert to avoid duplicate key

--- 1.7/module/give_all_groups_access_to_all_modules.sql
+++ 1.7/module/give_all_groups_access_to_all_modules.sql
@@ -1,4 +1,6 @@
-INSERT INTO `ps_module_group` 
+TRUNCATE `ps_module_group`;
+
+INSERT INTO `ps_module_group`
 SELECT m.`id_module`, s.`id_shop`, g.`id_group`
 FROM `ps_module` m, `ps_group` g, `ps_shop` s
-ORDER BY m.`id_module`
+ORDER BY m.`id_module`;
