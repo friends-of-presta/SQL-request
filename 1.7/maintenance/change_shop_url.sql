931ea0856850012fa99948d77798e5e218117692
Web Helpers Pau <75366465+WebHelpersPau@users.noreply.github.com>
2021-03-24 13:42:27 +0100

    Update change_shop_url.sql

--- 1.7/maintenance/change_shop_url.sql
+++ 1.7/maintenance/change_shop_url.sql
@@ -1,5 +1,9 @@
-UPDATE `ps_configuration` SET `value` = "www.newurl.ext" WHERE `name` = "PS_SHOP_DOMAIN";
-UPDATE `ps_configuration` SET `value` = "www.newurl.ext" WHERE `name` = "PS_SHOP_DOMAIN_SSL";
-UPDATE `ps_shop_url` SET `domain` = "www.newurl.ext", `domain_ssl` = "www.newurl.ext" WHERE `id_shop_url` = 1;
--- To activate SSL put value = 1 if not value = 0
-UPDATE `ps_configuration` SET `value` = "1" WHERE `name` IN ("PS_SSL_ENABLED", "PS_SSL_ENABLED_EVERYWHERE");
+-- URL of the website front
+SET @domain = "www.newurl.ext";
+-- Put sslon = 1 to enable SSL, and 0 to disable SSL
+SET @sslon = 1;
+
+UPDATE `ps_configuration` SET `value` = @domain WHERE `name` = "PS_SHOP_DOMAIN";
+UPDATE `ps_configuration` SET `value` = @domain WHERE `name` = "PS_SHOP_DOMAIN_SSL";
+UPDATE `ps_shop_url` SET `domain` = @domain , `domain_ssl` = @domain WHERE `id_shop_url` = 1;
+UPDATE `ps_configuration` SET `value` = @sslon WHERE `name` IN ("PS_SSL_ENABLED", "PS_SSL_ENABLED_EVERYWHERE");
