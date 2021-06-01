4668a741e117c0634c19ff57154b0f59c61e0019
PascalNetenvie <pascal@netenvie.com>
2021-06-01 15:15:06 +0200

    Create clean_stats_logs_v2.sql
    Précisez les variables en début de script et exécutez le SQL.
    Vide les tables ps_connections, ps_connections_page, ps_connections_source, ps_guest, ps_page_viewed, ps_referrer_cache, ps_pagenotfound.
    Et efface les enregistrements plus vieux que x jours des tables ps_mail, ps_log, ps_statssearch.

---
+++ 1.6/maintenance/clean_stats_logs_v2.sql
@@ -0,0 +1,45 @@
+# Prefixe des tables
+SET @prefix = 'ps';
+# Nombre de jours d'historiques à conserver
+SET @nbjour = 100;
+# Constitution et execution des requêtes
+SET @sql_text1 = concat('TRUNCATE TABLE ',@prefix,'_connections');
+PREPARE stmt1 FROM @sql_text1;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+SET @sql_text1 = concat('TRUNCATE TABLE ',@prefix,'_connections_page');
+PREPARE stmt1 FROM @sql_text1;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+SET @sql_text1 = concat('TRUNCATE TABLE ',@prefix,'_connections_source');
+PREPARE stmt1 FROM @sql_text1;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+SET @sql_text1 = concat('TRUNCATE TABLE ',@prefix,'_guest');
+PREPARE stmt1 FROM @sql_text1;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+SET @sql_text1 = concat('TRUNCATE TABLE ',@prefix,'_page_viewed');
+PREPARE stmt1 FROM @sql_text1;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+SET @sql_text1 = concat('TRUNCATE TABLE ',@prefix,'_referrer_cache');
+PREPARE stmt1 FROM @sql_text1;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+SET @sql_text1 = concat('TRUNCATE TABLE ',@prefix,'_pagenotfound');
+PREPARE stmt1 FROM @sql_text1;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+SET @sql_text1 = concat('DELETE FROM ',@prefix,'_mail',' WHERE date_add < now() - interval ',@nbjour,' DAY');
+PREPARE stmt1 FROM @sql_text1;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+SET @sql_text1 = concat('DELETE FROM ',@prefix,'_log',' WHERE date_add < now() - interval ',@nbjour,' DAY');
+PREPARE stmt1 FROM @sql_text1;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+SET @sql_text1 = concat('DELETE FROM ',@prefix,'_statssearch',' WHERE date_add < now() - interval ',@nbjour,' DAY');
+PREPARE stmt1 FROM @sql_text1;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
