33a6fc2e721860e5ccbd92b538e8a3ab0bb4e3d6
PascalNetenvie <pascal@netenvie.com>
2021-06-01 16:10:31 +0200

    Update clean_stats_logs.sql
    Conservation de l'historique (100 jours) sur les tables où c'est possible.

--- 1.6/maintenance/clean_stats_logs.sql
+++ 1.6/maintenance/clean_stats_logs.sql
@@ -1,10 +1,10 @@
-TRUNCATE TABLE `ps_connections`;
+DELETE FROM `ps_connections` WHERE date_add < now() - interval 100 DAY;
 TRUNCATE TABLE `ps_connections_page`;
-TRUNCATE TABLE `ps_connections_source`;
+DELETE FROM `ps_connections_source` WHERE date_add < now() - interval 100 DAY;
 TRUNCATE TABLE `ps_guest`;
+DELETE FROM `ps_pagenotfound` WHERE date_add < now() - interval 100 DAY;;
 TRUNCATE TABLE `ps_page_viewed`;
 TRUNCATE TABLE `ps_referrer_cache`;
-TRUNCATE TABLE `ps_pagenotfound`;
 DELETE FROM ps_mail WHERE date_add < now() - interval 100 DAY;
 DELETE FROM ps_log WHERE date_add < now() - interval 100 DAY;
 DELETE FROM ps_statssearch WHERE date_add < now() - interval 100 DAY;
