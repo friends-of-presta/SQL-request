23ca1334793332457be097ce64ef38cea94705df
PascalNetenvie <pascal@netenvie.com>
2021-09-10 11:48:09 +0200

    Update clean_stats_logs_v2.sql
    Rajout d'une requête pour supprimer les produits de paniers orphelins.

--- 1.6/maintenance/clean_stats_logs_v2.sql
+++ 1.6/maintenance/clean_stats_logs_v2.sql
@@ -64,6 +64,12 @@ PREPARE stmt1 FROM @sql_text1;
 EXECUTE stmt1;
 DEALLOCATE PREPARE stmt1;
 #
+# Suppression des produits de paniers orphelins
+SET @sql_text1 = concat('DELETE FROM ',@prefix,'_cart_product WHERE id_cart NOT IN (SELECT id_cart FROM ',@prefix,'_cart)');
+PREPARE stmt1 FROM @sql_text1;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+#
 # Suppression des invités sans commandes
 SET @sql_text1 = concat('DELETE g FROM ',@prefix,'_guest',' as g LEFT JOIN ',@prefix,'_cart as c ON g.id_guest = c.id_guest WHERE id_cart IS NULL');
 PREPARE stmt1 FROM @sql_text1;
