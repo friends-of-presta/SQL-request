ae58855945a2a4ab2d4d3a9c98c9a21f91418150
Jean-François Viguier <16720275+jf-viguier@users.noreply.github.com>
2021-10-22 18:47:00 +0200

    `

--- 1.7/maintenance/Attribute all groups to all categories.sql
+++ 1.7/maintenance/Attribute all groups to all categories.sql
@@ -1,1 +1,1 @@
-REPLACE INTO `ps_category_group` (`id_category`, `id_group`) (SELECT `id_category`, id_group FROM `ps_group`, `ps_category`)
+REPLACE INTO `ps_category_group` (`id_category`, `id_group`) (SELECT `id_category`, `id_group` FROM `ps_group`, `ps_category`)
