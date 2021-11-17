71cfd691972ecb4ab59d26c42408a4ed2f04fc89
olivier <olivier@ohweb.fr>
2021-11-17 09:09:20 +0100

    fix request, two left join with product_lang and add cl.id_lang

--- 1.7/products/1.7/products/export_all_products_with_images_features.sql
+++ 1.7/products/1.7/products/export_all_products_with_images_features.sql
@@ -4,7 +4,7 @@ SET @monsite = 'https://www.MONSITE.com/img/p/';
 
 SELECT
 p.`id_product` AS `ID`,
-b.name as `Nom`,`reference`,
+pl.name as `Nom`,`reference`,
 cl.`id_category` AS `ID Cat defaut`,
 cl.`name` AS `Cat defaut`,
 GROUP_CONCAT(DISTINCT(c.id_category) SEPARATOR ",") as categories,
@@ -57,22 +57,20 @@ concat(@monsite,insert(im.`id_image`,2,0,'/'),im.`id_image`,'.jpg')
 else ''
 end as "ImgURL_1"
 FROM `ps_product` p
-LEFT JOIN `ps_product_lang` pl ON (p.id_product = pl.id_product)
 LEFT JOIN `ps_category_product` cp ON (p.id_product = cp.id_product)
-LEFT JOIN `ps_category_lang` cl ON (cp.id_category = cl.id_category)
+LEFT JOIN `ps_category_lang` cl ON (cp.id_category = cl.id_category  AND cl.id_lang = @id_lang)
 LEFT JOIN `ps_category` c ON (cp.id_category = c.id_category)
-LEFT JOIN `ps_product_lang` b ON (b.`id_product` = p.`id_product` AND b.`id_lang` = @id_lang AND b.`id_shop` = p.id_shop_default)
+LEFT JOIN `ps_product_lang` pl ON (pl.`id_product` = p.`id_product` AND pl.`id_lang` = @id_lang AND pl.`id_shop` = p.id_shop_default)
 LEFT JOIN `ps_image` i ON (i.`id_product` = p.`id_product`)
 LEFT JOIN `ps_manufacturer_lang` ml ON (p.id_manufacturer = ml.id_manufacturer)
-LEFT JOIN `ps_stock_available` sav ON (sav.`id_product` = p.`id_product` AND sav.`id_product_attribute` = 0 AND sav.id_shop_group = 0 AND sav.id_shop = @id_shop ) 
+LEFT JOIN `ps_stock_available` sav ON (sav.`id_product` = p.`id_product` AND sav.`id_product_attribute` = 0 AND sav.id_shop_group = 0 AND sav.id_shop = @id_shop )
 LEFT JOIN `ps_product_shop` sa ON (p.`id_product` = sa.`id_product` AND sa.id_shop = p.id_shop_default)
 LEFT JOIN `ps_shop` shop ON (shop.id_shop = p.id_shop_default)
 LEFT JOIN `ps_image_shop` image_shop ON (image_shop.`id_image` = i.`id_image` AND image_shop.`cover` = 1 AND image_shop.id_shop = p.id_shop_default)
 LEFT JOIN `ps_product_download` pd ON (pd.`id_product` = p.`id_product`)
 LEFT JOIN `ps_image` im on im.`id_product`= p.`id_product`
-LEFT JOIN `ps_feature_product` fp ON fp.`id_product`= p.`id_product` 
+LEFT JOIN `ps_feature_product` fp ON fp.`id_product`= p.`id_product`
 LEFT JOIN `ps_feature_lang` fl ON (fp.id_feature = fl.id_feature AND fl.id_lang=@id_lang)
 LEFT JOIN `ps_feature_value_lang` fvl ON (fp.id_feature_value = fvl.id_feature_value AND fl.id_lang=@id_lang)
-WHERE pl.id_lang = @id_lang
 GROUP BY sa.id_product
 ORDER BY p.`id_product` ASC
