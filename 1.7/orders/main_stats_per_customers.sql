462790c56fa30a307d10ae2406d7c75105f6a126
Mickaël Andrieu <mickael.andrieu@solvolabs.com>
2022-02-18 11:02:58 +0100

    Added main stats about orders

---
+++ 1.7/orders/main_stats_per_customers.sql
@@ -0,0 +1,12 @@
+# Total spent, Profits and Orders per Customer
+SELECT firstname,
+	lastname,
+	email,
+	SUM(ROUND(od.total_price_tax_incl, 2)) AS total_spent,
+	SUM(ROUND((od.unit_price_tax_excl - od.original_wholesale_price) * od.product_quantity, 2)) AS profits,
+	COUNT(o.id_order) AS orders
+FROM ps_customer c
+	JOIN ps_orders o ON c.id_customer = o.id_customer
+	JOIN ps_order_detail od ON od.id_order = o.id_order
+GROUP BY c.id_customer
+;
